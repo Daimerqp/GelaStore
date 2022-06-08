b90a3e6d72b2e29a5af940980c6b2dbbb9e9ccea
unknown <gcristian5310@gmail.com>
2022-06-07 19:36:15 -0500

    intento

--- resources/views/Prueba.blade.php
+++ resources/views/Prueba.blade.php
@@ -6,11 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Prueba</title>
 
-    <!--Boostrap-->
+    <!--Bootstrap-->
     <link rel="stylesheet" href="./css/boostrap.min.css">
-    <script src="./js/boostrap.bundle.min.js"></script>
 </head>
 <body>
+    <div class="contaiber-fluid">
+        <nav class="navbar navbar-expand-md navbar-dark bg-black border-3 border-bottom border-primary">
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand">GELA´STORE</a>
+            </div>
+        </nav>
+    </div>
+
+    <script src="./js/boostrap.bundle.min.js"></script>
     
 </body>
 </html>
