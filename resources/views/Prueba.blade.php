2fb561e4dc48409dc73f5fdadf720449d11cff02
unknown <gcristian5310@gmail.com>
2022-06-07 19:14:53 -0500

    add librerias boostrap

---
+++ resources/views/Prueba.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prueba</title>
+
+    <!--Boostrap-->
+    <link rel="stylesheet" href="./css/boostrap.min.css">
+    <script src="./js/boostrap.bundle.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
